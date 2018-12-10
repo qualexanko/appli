9a6e69028b4720d8aaa44f4906c8a0a82228f10c
Quentin <quentin.alexanko@gmail.com>
2018-12-10 10:29:58 +0100

    externalisation des script

---
+++ assets/javascript/main.js
@@ -0,0 +1,8 @@
+/**
+ * @name main.js
+ * @description point d'entrée principal dans l'application javascript
+ */
+
+let title = document.getElementById('main-title');
+title.innerHTML = 'Hello Javascript';
+
